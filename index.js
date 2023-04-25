df9ed24c924ddedf5443337f90e1039f22094dfc
TherenceFrn <therence.ferron@orange.fr>
2023-04-25 22:06:27 +0200

    feat: cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3001 ;
 const hostname = "127.0.0.1"
+const cors = require('cors');
+
+require('dotenv').config()
+
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors());
 
 const indexRouter = require('./routes/index');
 
--- routes/index.js
+++ routes/index.js
@@ -6,75 +6,72 @@ const fs = require('fs');
 app.post('/css', (req, res) => {
   const params = req.body;
 
-  console.log(params)
-  
-  const cssContent = `
-    :root {
-      --primary: ${params.colors.primary.default};
-      --primary-darken: ${params.colors.primary.darken};
-      --primary-lighten: ${params.colors.primary.lighten};
-      --dark: ${params.colors.dark.default};
-      --dark-darken: ${params.colors.dark.darken};
-      --dark-lighten: ${params.colors.dark.lighten};
-      --light: ${params.colors.light.default};
-      --light-darken: ${params.colors.light.darken};
-      --light-lighten: ${params.colors.light.lighten};
-    }
-    
-    .container {
-      max-width: ${params.container.maxWidth}px;
-      padding: ${params.container.padding}px;
-    }
-    
-    h1 {
-      font-family: ${params.fonts.titles.family};
-      font-size:  ${params.fonts.titles.h1.size}px;
-      font-weight:  ${params.fonts.titles.h1.weight};
-    }
-    
-    h2 {
-      font-family: ${params.fonts.titles.family};
-      font-size:  ${params.fonts.titles.h2.size}px;
-      font-weight:  ${params.fonts.titles.h2.weight};
-    }
-    
-    h3 {
-      font-family: ${params.fonts.titles.family};
-      font-size:  ${params.fonts.titles.h3.size}px;
-      font-weight:  ${params.fonts.titles.h3.weight};
-    }
-    
-    h4 {
-      font-family: ${params.fonts.titles.family};
-      font-size:  ${params.fonts.titles.h4.size}px;
-      font-weight:  ${params.fonts.titles.h4.weight};
-    }
-    
-    h5 {
-      font-family: ${params.fonts.titles.family};
-      font-size:  ${params.fonts.titles.h5.size}px;
-      font-weight:  ${params.fonts.titles.h5.weight};
-    }
-    
-    h6 {
-      font-family: ${params.fonts.titles.family};
-      font-size:  ${params.fonts.titles.h6.size}px;
-      font-weight:  ${params.fonts.titles.h6.weight};
-    }
-    
-    p {
-      font-family: ${params.fonts.paragraphs.family};
-      font-size: ${params.fonts.paragraphs.size}px;
-    }
-    
-    button {
-      background-color: ${params.button.backgroundColor};
-      margin: ${params.button.margin}px;
-      border-radius: ${params.button.borderRadius}px;
-      font-size: ${params.button.fontSize}px;
-    }
-   
-    /* Autres règles CSS à ajouter */
+  const cssContent = `:root {
+  --primary: ${params.colors.primary.default};
+  --primary-darken: ${params.colors.primary.darken};
+  --primary-lighten: ${params.colors.primary.lighten};
+  --dark: ${params.colors.dark.default};
+  --dark-darken: ${params.colors.dark.darken};
+  --dark-lighten: ${params.colors.dark.lighten};
+  --light: ${params.colors.light.default};
+  --light-darken: ${params.colors.light.darken};
+  --light-lighten: ${params.colors.light.lighten};
+}
+
+.container {
+  max-width: ${params.container.maxWidth}px;
+  padding: ${params.container.padding}px;
+}
+
+h1 {
+  font-family: ${params.fonts.titles.family};
+  font-size:  ${params.fonts.titles.h1.size}px;
+  font-weight:  ${params.fonts.titles.h1.weight};
+}
+
+h2 {
+  font-family: ${params.fonts.titles.family};
+  font-size:  ${params.fonts.titles.h2.size}px;
+  font-weight:  ${params.fonts.titles.h2.weight};
+}
+
+h3 {
+  font-family: ${params.fonts.titles.family};
+  font-size:  ${params.fonts.titles.h3.size}px;
+  font-weight:  ${params.fonts.titles.h3.weight};
+}
+
+h4 {
+  font-family: ${params.fonts.titles.family};
+  font-size:  ${params.fonts.titles.h4.size}px;
+  font-weight:  ${params.fonts.titles.h4.weight};
+}
+
+h5 {
+  font-family: ${params.fonts.titles.family};
+  font-size:  ${params.fonts.titles.h5.size}px;
+  font-weight:  ${params.fonts.titles.h5.weight};
+}
+
+h6 {
+  font-family: ${params.fonts.titles.family};
+  font-size:  ${params.fonts.titles.h6.size}px;
+  font-weight:  ${params.fonts.titles.h6.weight};
+}
+
+p {
+  font-family: ${params.fonts.paragraphs.family};
+  font-size: ${params.fonts.paragraphs.size}px;
+}
+
+button {
+  background-color: ${params.button.backgroundColor};
+  margin: ${params.button.margin}px;
+  border-radius: ${params.button.borderRadius}px;
+  font-size: ${params.button.fontSize}px;
+}
+
+/* Autres règles CSS à ajouter */
   `;
 
   const tmpFilePath = path.join(__dirname, 'temp.css');
