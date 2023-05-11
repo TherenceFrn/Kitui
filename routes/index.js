ef2167d75d1ec1d5db7608d52a58988b69527728
TherenceFrn <therence.ferron@orange.fr>
2023-05-11 20:42:54 +0200

    feat: change file format

--- routes/index.js
+++ routes/index.js
@@ -139,7 +139,7 @@ function getCurrentDateTime() {
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const seconds = String(now.getSeconds()).padStart(2, '0');
 
-  return `${year}${month}${day}-${hours}${minutes}${seconds}`;
+  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
 }
 
 module.exports = app;
